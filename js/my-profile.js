96c96cbed6f788f9a98c2fafb459a4a40bff6383
giulianoyori <68905402+giulianoyori@users.noreply.github.com>
2020-11-20 10:42:17 -0300

    entrega 8

--- js/my-profile.js
+++ js/my-profile.js
@@ -1,10 +1,94 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function (e) {
+let usuario2 = document.getElementById("usuario2");
+let nombreUsuario = document.getElementById("nombreUsuario");
+let apellidoUsuario = document.getElementById("apellidoUsuario");
+let edadUsuario = document.getElementById("edadUsuario");
+let emailUsuario = document.getElementById("emailUsuario");
+let celularUsuario = document.getElementById("celularUsuario");
+let imagenUsuarioURL = document.getElementById("URLusuario");
+let imagenUsuario = document.getElementById("imagenUsuario");
 
+let botonEditar = document.getElementById("botonEditar");
+let botonGuardar = document.getElementById("botonGuardar");
+let botonCancelar = document.getElementById("botonCancelar");
+
+document.addEventListener("DOMContentLoaded", function (e) {
+    usuario2.value = info;
+    if (localStorage.getItem("datosUsuario") != null) {
+        let usuarioobj = JSON.parse(localStorage.getItem("datosUsuario"));
+
+        usuario2.value = usuarioobj.usuario
+        localStorage.setItem("user", usuarioobj.usuario);
+
+        nombreUsuario.value = usuarioobj.nombre;
+        apellidoUsuario.value = usuarioobj.apellido;
+        edadUsuario.value = usuarioobj.edad;
+        emailUsuario.value = usuarioobj.mail;
+        celularUsuario.value = usuarioobj.celular;
+       imagenUsuarioURL.value = usuarioobj.imagen;
+       //imagenUsuario.setAttribute("src", imagenUsuarioURL.value);
+    imagenUsuario.src = imagenUsuarioURL.value;
+
+
+    }
 
 });
 
 
 
+function editarDatos() {
+    if (info == null) {
+        alert("Debe iniciar sesión para modificar los datos de usuario");
+        window.location.href = "index(login).html"
+    }
+    if (info != null) {
+        usuario2.value = info;
+        usuario2.removeAttribute("disabled");
+        nombreUsuario.removeAttribute("disabled");
+        apellidoUsuario.removeAttribute("disabled");
+        edadUsuario.removeAttribute("disabled");
+        emailUsuario.removeAttribute("disabled");
+        celularUsuario.removeAttribute("disabled");
+        imagenUsuarioURL.removeAttribute("disabled");
+        botonEditar.style.display = "none";
+        botonGuardar.style.display = "block";
+        
+
+    }
+}
+
+function guardarDatos() {
+    
+    if (usuario2.value == "")
+    {alert("El usuario no puede ser nulo. Elija un nuevo nombre de usuario");}
+    else{
+    let objUsuario = {
+        "usuario": usuario2.value, "nombre": nombreUsuario.value, "apellido": apellidoUsuario.value, "edad": edadUsuario.value, "mail": emailUsuario.value, "celular": celularUsuario.value, "imagen": imagenUsuarioURL.value
+    }
+    let jsonUsuario = JSON.stringify(objUsuario);
+
+    localStorage.setItem("datosUsuario", jsonUsuario);
+
+
+    localStorage.setItem("user", usuario2.value);
+    info = localStorage.getItem("user");
+    document.getElementById("nombrebar").innerHTML = info;
+
+    usuario2.disabled = true;
+    nombreUsuario.disabled = true;
+    apellidoUsuario.disabled = true;
+    edadUsuario.disabled = true;
+    emailUsuario.disabled = true;
+    celularUsuario.disabled = true;
+    imagenUsuarioURL.disabled = true
+    
+    botonEditar.style.display = "block";
+    botonGuardar.style.display = "none";
+    if (imagenUsuarioURL.value != ""){
+        imagenUsuario.src = imagenUsuarioURL.value;  
+    }
+    
+
+}}
--- js/products.js
+++ js/products.js
@@ -3,82 +3,78 @@
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function (e) {
 
-  getJSONData(PRODUCTS_URL).then(function(resultObj){
-    if (resultObj.status === "ok")
-    {
-        productsArray = resultObj.data;
-        //Muestro las categorías ordenadas
-        showProductsList(productsArray);
+  getJSONData(PRODUCTS_URL).then(function (resultObj) {
+    if (resultObj.status === "ok") {
+      productsArray = resultObj.data;
+      //Muestro las categorías ordenadas
+      showProductsList(productsArray);
     }
-});
+  });
 
- 
+
 
 });
 
 var productsArray = [];
 //FUNCIÓN QUE GENERA UNA PORCIÓN DE HTML Y LA AÑADE A NUESTRO DOCUMENTO MOSTRANDO CADA  ARTÍCULO CON UN CICLO: "FOR"
 //TOMA LOS DATOS DE LOS ARTÍCULOS DEL ARRAY OBTENIDO  DE LA URL MEDIANTE EL "getJSONData" 
-function showProductsList(array){
+function showProductsList(array) {
 
-    let htmlContentToAppend = "";
-    for(let i = 0; i < array.length; i++){
-        let product = array[i];
+  let htmlContentToAppend = "";
+  for (let i = 0; i < array.length; i++) {
+    let product = array[i];
 
-        htmlContentToAppend += `
-        <a href="product-info.html" class="list-group-item list-group-item-action">
-        <div class="list-group-item list-group-item-action">
-            <div  class="row">
-                <div class="col-3">
-                    <img src="` + product.imgSrc + `" alt="` + product.description + `" class="img-thumbnail">
-                </div>
-                <div class="col">
-                    <div class="d-flex w-100 justify-content-between">
-                        <h4 class="mb-1">`+ product.name +`</h4>
-                        
-                        <p class="text-muted">` + product.currency + " " + product.cost + " </br>" + product.soldCount +` Unidades vendidas</p>
-                    </div>
-                 <p>` + product.description + `</p>
-                
-                </div>
-            </div>
-        </div>
+    htmlContentToAppend += `
+     
+
+        <div class="col-4">
+        <a href="product-info.html" class="card mb-4 shadow-sm custom-card">
+          <img class="bd-placeholder-img card-img-top" src="${product.imgSrc}">
+          <h3 class="m-3">${product.name} </h3>
+          <div class="card-body">
+            <p class="card-text">${product.description}</p>
+          </div>
+          <h4 class="m-3 text-right">${product.currency + "  " + product.cost} </h4>
+          <h5 class="m-3 text-right text-muted">${product.soldCount} Unidades vendidas</h5>
+
         </a>
+    </div>
+
+
         `
 
-        document.getElementById("contenedorDestino").innerHTML = htmlContentToAppend;
-    }
+    document.getElementById("contenedorDestino").innerHTML = htmlContentToAppend;
+  }
 }
 
 
 //fUNCIÓN QUE ORDENA AL ARRAY DE LOS PRODUCTOS SEGÚN EL CRITERIO MEDIANTE EL MÉTODO: "SORT"
 //LUEGO MUESTRA EL ARRAY YA ORDENADO CON LA FUNCIÓN: "showProductsList"
 
-function ordenar(){
-    let criterios = document.getElementById('orden');
-    let criterio = criterios.value;
-   
-  if (criterio === "mayorprecio")
-  {
-     productsArray.sort(function (prev, next){
-       return next.cost - prev.cost;
-     });
-     showProductsList(productsArray);
-     
-    }
-    if(criterio === "menorprecio"){
-      productsArray.sort(function (prev, next){
-        return prev.cost - next.cost;
-      });
-      showProductsList(productsArray);
-    }
-  if(criterio === "relevancia"){
-    productsArray.sort(function (prev, next){
+function ordenar() {
+  let criterios = document.getElementById('orden');
+  let criterio = criterios.value;
+
+  if (criterio === "mayorprecio") {
+    productsArray.sort(function (prev, next) {
+      return next.cost - prev.cost;
+    });
+    showProductsList(productsArray);
+
+  }
+  if (criterio === "menorprecio") {
+    productsArray.sort(function (prev, next) {
+      return prev.cost - next.cost;
+    });
+    showProductsList(productsArray);
+  }
+  if (criterio === "relevancia") {
+    productsArray.sort(function (prev, next) {
       return next.soldCount - prev.soldCount;
     });
     showProductsList(productsArray);
   }
-  
+
 }
 ordenar();
 
@@ -89,17 +85,17 @@ let arrayEncontrados = [];
 
 //FUNCIÓN QUE FILTRA EL ARRAY DE PRODUCTOS SEGÚN LO QUE SE OBTIENE DEL INPUT DE BÚSQUEDA
 //LUEGO MUESTRA EL ARRAY YA FILLTRADO CON LA FUNCIÓN: "showProductsList"
-function buscar(){
+function buscar() {
   let barraBusqueda = document.getElementById("barraBusqueda").value.toLowerCase();
-  arrayEncontrados = productsArray.filter((product)=>{
+  arrayEncontrados = productsArray.filter((product) => {
     return (product.name.toLowerCase().includes(barraBusqueda) || product.description.toLowerCase().includes(barraBusqueda));
 
   })
-  if (arrayEncontrados.length == 0){
+  if (arrayEncontrados.length == 0) {
     document.getElementById("contenedorDestino").innerHTML = "No hay productos que coincidan con sus criterios de búsqueda";
   }
-  else{
-showProductsList(arrayEncontrados);
+  else {
+    showProductsList(arrayEncontrados);
   }
 }
 
@@ -107,17 +103,17 @@ showProductsList(arrayEncontrados);
 //FUNCIÓN QUE OBTINE PARÁMETROS DE MAYOR Y MENOR PRECIO Y FILTRA EL ARRAY DE PRODUCTOS SEGÚN 
 //SU PRECIO. DEBE ESTAR DENTRO DEL RANGO
 //LUEGO MUESTRA EL ARRAY YA FILLTRADO CON LA FUNCIÓN: "showProductsList"
-function rangoPrecios(){
+function rangoPrecios() {
   let precioMenor = document.getElementById("preciomenor").value;
   let precioMayor = document.getElementById("preciomayor").value;
-  
-  arrayEncontrados = productsArray.filter((product)=>{
-    return(product.cost >= precioMenor && product.cost <= precioMayor);
+
+  arrayEncontrados = productsArray.filter((product) => {
+    return (product.cost >= precioMenor && product.cost <= precioMayor);
   })
-  if (arrayEncontrados.length == 0){
+  if (arrayEncontrados.length == 0) {
     document.getElementById("contenedorDestino").innerHTML = "No hay productos que coincidan con sus criterios de búsqueda";
   }
-  else{
-  showProductsList(arrayEncontrados);
+  else {
+    showProductsList(arrayEncontrados);
   }
 }
